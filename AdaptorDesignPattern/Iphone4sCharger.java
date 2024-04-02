149b484c79fc9605a9d0578dc9e77c76f5e6dc88
Bhagyashri Patil <bhagyashri156039@gmail.com>
2024-04-02 07:26:50 +0530

    adapter design pattern implemented

---
+++ AdaptorDesignPattern/Iphone4sCharger.java
@@ -0,0 +1,8 @@
+package AdaptorDesignPattern;
+
+public class Iphone4sCharger implements Charger{
+    @Override
+    public void charge() {
+        System.out.println("charging with 4s charger");
+    }
+}
